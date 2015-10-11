c9cddd3f0d346e940c169e8cde5aeef5c709f3ff
madsbf <madsbf@gmail.com>
2015-10-11 13:56:05 +0200

    Improved app swatches for better text readability

--- app/src/main/kotlin/me/madsbf/launcher/model/entities/AppSwatch.kt
+++ app/src/main/kotlin/me/madsbf/launcher/model/entities/AppSwatch.kt
@@ -1,5 +1,6 @@
 package me.madsbf.launcher.model.entities
 
+import android.graphics.Color
 import android.support.v7.graphics.Palette
 import me.madsbf.launcher.view.utils.PaletteUtils
 
@@ -9,6 +10,35 @@ public class AppSwatch(palette: Palette) {
 
     init {
         val swatch = PaletteUtils.getMostVibrantSwatch(palette)
-        textColor = swatch.rgb
+        var red = Color.red(swatch.rgb)
+        var green = Color.green(swatch.rgb)
+        var blue = Color.blue(swatch.rgb)
+        val total = blue + red + green
+
+        // Check if text is too bright
+        if(total > 510) {
+            val change = (total - 510) / 3
+            red -= change
+            green -= change
+            blue -= change
+
+            red = Math.max(0, red)
+            green = Math.max(0, green)
+            blue = Math.max(0, blue)
+        }
+
+        // Special brightness case for green/yellow
+        if(green > 200 && total > 360) {
+            val change = (total - 360) / 3
+            red -= change
+            green -= change
+            blue -= change
+
+            red = Math.max(0, red)
+            green = Math.max(0, green)
+            blue = Math.max(0, blue)
+        }
+
+        textColor = Color.rgb(red, green, blue)
     }
 }
